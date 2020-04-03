45ec3435c4774c4be113bb92220a68bde4f6bc2f
tinkukosta <49283381+tinkukosta@users.noreply.github.com>
2020-04-03 19:40:43 +0530

    Add files via upload

---
+++ Internet_mannual_control_with_feedback.ino
@@ -0,0 +1,238 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+BlynkTimer timer;
+
+
+#define DEBUG_SW 0
+
+
+#define S1 D0
+#define R1 D1
+
+#define S2 D2
+#define R2 D3
+
+#define S3 D4
+#define R3 D5
+
+#define S4 D6
+#define R4 D7
+
+
+
+
+int MODE = 0;
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "SSID";
+char pass[] = "PASS";
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "AUTH TOKEN";
+
+
+int switch_ON_Flag1_previous_I = 0;
+int switch_ON_Flag2_previous_I = 0;
+int switch_ON_Flag3_previous_I = 0;
+int switch_ON_Flag4_previous_I = 0;
+
+
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  digitalWrite(R1, pinValue);
+  // process received value
+}
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V2 to a variable
+  digitalWrite(R2, pinValue);
+  // process received value
+}
+
+BLYNK_WRITE(V3)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V3 to a variable
+  digitalWrite(R3, pinValue);
+  // process received value
+}
+
+BLYNK_WRITE(V4)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V4 to a variable
+  digitalWrite(R4, pinValue);
+  // process received value
+}
+
+void with_internet()
+{
+  if (digitalRead(S1) == LOW)
+  {
+    if (switch_ON_Flag1_previous_I == 0 )
+    {
+      digitalWrite(R1, LOW);
+      if (DEBUG_SW) Serial.println("Relay1- ON");
+      Blynk.virtualWrite(V1, 0);
+      switch_ON_Flag1_previous_I = 1;
+    }
+    if (DEBUG_SW) Serial.println("Switch1 -ON");
+
+  }
+  if (digitalRead(S1) == HIGH )
+  {
+    if (switch_ON_Flag1_previous_I == 1)
+    {
+      digitalWrite(R1, HIGH);
+      if (DEBUG_SW) Serial.println("Relay1 OFF");
+      Blynk.virtualWrite(V1, 1);
+      switch_ON_Flag1_previous_I = 0;
+    }
+    if (DEBUG_SW)Serial.println("Switch1 OFF");
+  }
+
+
+  if (digitalRead(S2) == LOW)
+  {
+    if (switch_ON_Flag2_previous_I == 0 )
+    {
+      digitalWrite(R2, LOW);
+      if (DEBUG_SW)  Serial.println("Relay2- ON");
+      Blynk.virtualWrite(V2, 0);
+      switch_ON_Flag2_previous_I = 1;
+    }
+    if (DEBUG_SW) Serial.println("Switch2 -ON");
+
+  }
+  if (digitalRead(S2) == HIGH )
+  {
+    if (switch_ON_Flag2_previous_I == 1)
+    {
+      digitalWrite(R2, HIGH);
+      if (DEBUG_SW) Serial.println("Relay2 OFF");
+      Blynk.virtualWrite(V2, 1);
+      switch_ON_Flag2_previous_I = 0;
+    }
+    if (DEBUG_SW)Serial.println("Switch2 OFF");
+    //delay(200);
+  }
+
+  if (digitalRead(S3) == LOW)
+  {
+    if (switch_ON_Flag3_previous_I == 0 )
+    {
+      digitalWrite(R3, LOW);
+      if (DEBUG_SW) Serial.println("Relay3- ON");
+      Blynk.virtualWrite(V3, 0);
+      switch_ON_Flag3_previous_I = 1;
+    }
+    if (DEBUG_SW) Serial.println("Switch3 -ON");
+
+  }
+  if (digitalRead(S3) == HIGH )
+  {
+    if (switch_ON_Flag3_previous_I == 1)
+    {
+      digitalWrite(R3, HIGH);
+      if (DEBUG_SW) Serial.println("Relay3 OFF");
+      Blynk.virtualWrite(V3, 1);
+      switch_ON_Flag3_previous_I = 0;
+    }
+    if (DEBUG_SW)Serial.println("Switch3 OFF");
+    //delay(200);
+  }
+
+  if (digitalRead(S4) == LOW)
+  {
+    if (switch_ON_Flag4_previous_I == 0 )
+    {
+      digitalWrite(R4, LOW);
+      if (DEBUG_SW) Serial.println("Relay4- ON");
+      Blynk.virtualWrite(V4, 0);
+      switch_ON_Flag4_previous_I = 1;
+    }
+    if (DEBUG_SW) Serial.println("Switch4 -ON");
+
+  }
+  if (digitalRead(S4) == HIGH )
+  {
+    if (switch_ON_Flag4_previous_I == 1)
+    {
+      digitalWrite(R4, HIGH);
+      if (DEBUG_SW) Serial.println("Relay4 OFF");
+      Blynk.virtualWrite(V4, 1);
+      switch_ON_Flag4_previous_I = 0;
+    }
+    if (DEBUG_SW)Serial.println("Switch4 OFF");
+    //delay(200);
+  }
+
+
+
+}
+
+void without_internet()
+{
+
+  digitalWrite(R1, digitalRead(S1));
+  digitalWrite(R2, digitalRead(S2));
+  digitalWrite(R3, digitalRead(S3));
+  digitalWrite(R4, digitalRead(S4));
+
+}
+
+
+void checkBlynk() { // called every 3 seconds by SimpleTimer
+
+  bool isconnected = Blynk.connected();
+  if (isconnected == false) {
+    MODE = 1;
+  }
+  if (isconnected == true) {
+    MODE = 0;
+  }
+}
+
+void setup()
+{
+  // Debug console
+  if (DEBUG_SW) Serial.begin(9600);
+  pinMode(S1, INPUT);
+  pinMode(R1, OUTPUT);
+
+  pinMode(S2, INPUT);
+  pinMode(R2, OUTPUT);
+
+  pinMode(S3, INPUT);
+  pinMode(R3, OUTPUT);
+
+  pinMode(S4, INPUT);
+  pinMode(R4, OUTPUT);
+  //pinMode(MODE, INPUT);
+  WiFi.begin(ssid, pass);
+  timer.setInterval(3000L, checkBlynk); // check if connected to Blynk server every 3 seconds
+  Blynk.config(auth);//, ssid, pass);
+
+}
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    if (DEBUG_SW) Serial.println("Not Connected");
+  }
+  else
+  {
+    if (DEBUG_SW) Serial.println(" Connected");
+    Blynk.run();
+  }
+
+  timer.run(); // Initiates SimpleTimer
+  if (MODE == 0)
+    with_internet();
+  else
+    without_internet();
+}
